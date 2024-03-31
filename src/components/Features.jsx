aee0835bb0d31e06f330bdbdf13f657630a3b466
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-03-31 21:56:23 +0530

    routing done

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,16 +1,17 @@
-// import React from 'react'
 import img1 from "../assets/img1.png";
 import img2 from "../assets/image2.jpg";
-import img3 from "../assets/img3.jpg"
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useAuth0 } from '@auth0/auth0-react';
+import img3 from "../assets/img3.jpg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+
+
 const Button = styled.button`
   padding: 12px 24px;
   font-size: 18px;
-  font-weight:20px;
-  background: #2B394A;
+  font-weight: 20px;
+  background: #2b394a;
   color: #fff;
   border: none;
   border-radius: 4px;
@@ -18,158 +19,173 @@ const Button = styled.button`
 
   &:hover {
     background: #00dada;
-    color:black;
-    font-weight:20px;
+    color: black;
+    font-weight: 20px;
   }
 `;
 const Services = styled.section`
-padding: 30px;
-padding-bottom: 150px;
-display: flex;
-justify-content: center;
-align-items: center;
-
-.heading{
-  font-size: 42px;
-  color: #ff6d6b;
-  font-weight:700;
-  margin-bottom: 48px;
-  text-shadow: 1px 1px 1px rgb(43 57 74);
-}
-.wrapper{
+  padding: 30px;
+  padding-bottom: 150px;
   display: flex;
-  justify-content: space-around ;
-}
-.card{
-  display: inline-block;
-  margin-left:10rem;
-  margin-top: 3rem;
-  width: 280px;
-  height: 360px;
-  padding: 2rem 1rem;
-  background: #fff;
-  position: relative;
-  align-items: flex-end;
-  box-shadow: 0px 7px 10px rgba(0,0,0,0.5);
-  transition: 0.5s ease-in-out;
-}
-.card:hover{
-  transform: translateY(20px);
-  cursor: pointer;
-}
-.card:before{
-  content: "";
-  position: absolute;
-  top: 0;
-  left:0;
-  display: block;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(to bottom, rgba(0,176,155,0.5), rgba(150,201,61,1));
-  z-index: 2;
-  transition: 0.5s all;
-  opacity: 0;
-}
-.card:hover:before{
-  opacity: 1;
-}
+  justify-content: center;
+  align-items: center;
 
-.card-img-top{
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  position: absolute;
-  top: 0;
-  left: 0;
+  .heading {
+    font-size: 42px;
+    color: #ff6d6b;
+    font-weight: 700;
+    margin-bottom: 48px;
+    text-shadow: 1px 1px 1px rgb(43 57 74);
+  }
+  .wrapper {
+    display: flex;
+    justify-content: space-around;
+  }
+  .card {
+    display: inline-block;
+    margin-left: 10rem;
+    margin-top: 3rem;
+    width: 280px;
+    height: 360px;
+    padding: 2rem 1rem;
+    background: #fff;
+    position: relative;
+    align-items: flex-end;
+    box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.5);
+    transition: 0.5s ease-in-out;
+  }
+  .card:hover {
+    transform: translateY(20px);
+    cursor: pointer;
+  }
+  .card:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(
+      to bottom,
+      rgba(0, 176, 155, 0.5),
+      rgba(150, 201, 61, 1)
+    );
+    z-index: 2;
+    transition: 0.5s all;
+    opacity: 0;
+  }
+  .card:hover:before {
+    opacity: 1;
+  }
 
-}
+  .card-img-top {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 
-.card-body{
-  position: relative;
-  z-index: 3;
-   color: #fff;
-  opacity: 0;
-  transform: translateY(30px);
-  transition: 0.5s all;
-}
+  .card-body {
+    position: relative;
+    z-index: 3;
+    color: #fff;
+    opacity: 0;
+    transform: translateY(30px);
+    transition: 0.5s all;
+  }
 
-.card:hover .card-body{
-  opacity: 1;
-  transform: translateY(0px);
-}
+  .card:hover .card-body {
+    opacity: 1;
+    transform: translateY(0px);
+  }
 
-.card .card-body h5{
-         margin: 0;
-}
-.card .card-body p{
-  letter-spacing: 1px;
-  font-size: 15px;
-  margin-top: 8px;
-  margin-bottom: 20px;
-}
-.btn btn-primary{
-  box-shadow: 0px 7px 10px rgba(0,0,0,0.5);
-}
+  .card .card-body h5 {
+    margin: 0;
+  }
+  .card .card-body p {
+    letter-spacing: 1px;
+    font-size: 15px;
+    margin-top: 8px;
+    margin-bottom: 20px;
+  }
+  .btn btn-primary {
+    box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.5);
+  }
 `;
 const Features = () => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
   return (
     // <div>Features</div>
     <>
-    <h2 className="heading">FEATURES</h2>
-    <Services id="featureSection">
-      <div className="wrapper">
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={img1} className="card-img-top"  />
-          <div className="card-body">
-            <h5 className="card-title">Reading Area</h5>
-            <p className="card-text">Ready to unwind and let loose? Discover actionable mental health and wellbeing advice.</p>
-            <Link to="/readingarea" target="_blank">
-              <Button>Read Now</Button>
-            </Link>
+      <h2 className="heading">FEATURES</h2>
+      <Services id="featureSection">
+        <div className="wrapper">
+          <div className="card" style={{ width: "18rem" }}>
+            <img src={img1} className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">Reading Area</h5>
+              <p className="card-text">
+                Ready to unwind and let loose? Discover actionable mental health
+                and wellbeing advice.
+              </p>
+              <Link to="/readingarea" target="_blank">
+                <Button>Read Now</Button>
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={img2} className="card-img-top"  />
-          <div className="card-body">
-            <h5 className="card-title">Explore Games</h5>
-            <p className="card-text">Experience the joy of play to relax and rejuvenate! Dive into our games for a refreshing escape.</p>
-            <Link to="/" target="_blank">
-              <Button>Play Now</Button>
-            </Link>
+          <div className="card" style={{ width: "18rem" }}>
+            <img src={img2} className="card-img-top" />
+            <div className="card-body">
+              <h5 className="card-title">Explore Games</h5>
+              <p className="card-text">
+                Experience the joy of play to relax and rejuvenate! Dive into
+                our games for a refreshing escape.
+              </p>
+              <Link to="/games" target="_blank">
+                <Button>Play Now</Button>
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={img3} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Chat Bot</h5>
-            <p className="card-text">Engage in a lively chat with our friendly bot for a fun and helpful interaction!</p>
-            <Link to="https://emowellbeing.streamlit.app/" target="_blank">
-              <Button>Chat Now</Button>
-            </Link>
-            {/* <a href="https://emowellbeing.streamlit.app/" className="btn btn-primary">Chat Now</a> */}
+          <div className="card" style={{ width: "18rem" }}>
+            <img src={img3} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Chat Bot</h5>
+              <p className="card-text">
+                Engage in a lively chat with our friendly bot for a fun and
+                helpful interaction!
+              </p>
+              <Link to="https://emowellbeing.streamlit.app/" target="_blank">
+                <Button>Chat Now</Button>
+              </Link>
+              {/* <a href="https://emowellbeing.streamlit.app/" className="btn btn-primary">Chat Now</a> */}
+            </div>
+          </div>
+          <div className="card">
+            <img src={img3} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Community Forum</h5>
+              <p className="card-text">
+                Engage in a lively chat with our friendly bot for a fun and
+                helpful interaction!
+              </p>
+              {isAuthenticated ? (
+                <Link to="./forum" target="_blank">
+                  <Button>Learn More</Button>
+                </Link>
+              ) : (
+                <Button onClick={() => loginWithRedirect()}>Learn More</Button>
+              )}
+            </div>
           </div>
         </div>
-        <div className="card">
-          <img src={img3} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Community Forum</h5>
-            <p className="card-text">Engage in a lively chat with our friendly bot for a fun and helpful interaction!</p>
-            {isAuthenticated ?(
-              <Link to="./forum" target="_blank">
-              <Button>Learn More</Button>
-            </Link>
-          ):(
-            <Button onClick={() => loginWithRedirect()}>Learn More</Button>
-          )}
-          </div>
-        </div>
-      </div>
-    </Services>
+      </Services>
     </>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
