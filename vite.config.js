1fc7490e2ed33a46a5a0c7ba04e4a4e3b1131f68
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-04-02 21:31:12 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  // optimizeDeps: {
-  //   exclude: ['nextjs-websocket']
-  // }
-
-})
+  resolve: {
+    // eslint-disable-next-line no-undef
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
+  },
+  // Fix large chunks issue due to node_modules
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        }
+      }
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  }
+});
