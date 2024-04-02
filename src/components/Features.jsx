4eed1d417e42e7e77d6241733e187ca5ef36d15a
Monalika Patnaik <99353300+MonalikaPatnaik@users.noreply.github.com>
2024-04-03 00:53:51 +0530

    forum-updated (#13)

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,200 +1,10 @@
 import img1 from "../assets/img1.png";
 import img2 from "../assets/image2.jpg";
 import img3 from "../assets/img-3.png";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import { useAuth0 } from "@auth0/auth0-react";
-// import { Link } from "react-router-dom";
-// import styled from "styled-components";
-
-
-// const Button = styled.button`
-//   padding: 12px 24px;
-//   font-size: 18px;
-//   font-weight: 20px;
-//   background: #2b394a;
-//   color: #fff;
-//   border: none;
-//   border-radius: 4px;
-//   cursor: pointer;
-
-//   &:hover {
-//     background: #00dada;
-//     color: black;
-//     font-weight: 20px;
-//   }
-// `;
-// const Services = styled.section`
-//   padding: 30px;
-//   padding-bottom: 150px;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-
-//   .heading {
-//     font-size: 42px;
-//     color: #ff6d6b;
-//     font-weight: 700;
-//     margin-bottom: 48px;
-//     text-shadow: 1px 1px 1px rgb(43 57 74);
-//   }
-//   .wrapper {
-//     display: flex;
-//     justify-content: space-around;
-//   }
-//   .card {
-//     display: inline-block;
-//     margin-left: 10rem;
-//     margin-top: 3rem;
-//     width: 280px;
-//     height: 360px;
-//     padding: 2rem 1rem;
-//     background: #fff;
-//     position: relative;
-//     align-items: flex-end;
-//     box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.5);
-//     transition: 0.5s ease-in-out;
-//   }
-//   .card:hover {
-//     transform: translateY(20px);
-//     cursor: pointer;
-//   }
-//   .card:before {
-//     content: "";
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//     display: block;
-//     width: 100%;
-//     height: 100%;
-//     background: linear-gradient(
-//       to bottom,
-//       rgba(0, 176, 155, 0.5),
-//       rgba(150, 201, 61, 1)
-//     );
-//     z-index: 2;
-//     transition: 0.5s all;
-//     opacity: 0;
-//   }
-//   .card:hover:before {
-//     opacity: 1;
-//   }
-
-//   .card-img-top {
-//     width: 100%;
-//     height: 100%;
-//     object-fit: cover;
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//   }
-
-//   .card-body {
-//     position: relative;
-//     z-index: 3;
-//     color: #fff;
-//     opacity: 0;
-//     transform: translateY(30px);
-//     transition: 0.5s all;
-//   }
-
-//   .card:hover .card-body {
-//     opacity: 1;
-//     transform: translateY(0px);
-//   }
-
-//   .card .card-body h5 {
-//     margin: 0;
-//   }
-//   .card .card-body p {
-//     letter-spacing: 1px;
-//     font-size: 15px;
-//     margin-top: 8px;
-//     margin-bottom: 20px;
-//   }
-//   .btn btn-primary {
-//     box-shadow: 0px 7px 10px rgba(0, 0, 0, 0.5);
-//   }
-// `;
-// const Features = () => {
-//   const { isAuthenticated, loginWithRedirect } = useAuth0();
-//   return (
-//     // <div>Features</div>
-//     <>
-//       <h2 className="heading">FEATURES</h2>
-//       <Services id="featureSection">
-//         <div className="wrapper">
-//           <div className="card" style={{ width: "18rem" }}>
-//             <img src={img1} className="card-img-top" />
-//             <div className="card-body">
-//               <h5 className="card-title">Reading Area</h5>
-//               <p className="card-text">
-//                 Ready to unwind and let loose? Discover actionable mental health
-//                 and wellbeing advice.
-//               </p>
-//               <Link to="/readingarea" target="_blank">
-//                 <Button>Read Now</Button>
-//               </Link>
-//             </div>
-//           </div>
-
-//           <div className="card" style={{ width: "18rem" }}>
-//             <img src={img2} className="card-img-top" />
-//             <div className="card-body">
-//               <h5 className="card-title">Explore Games</h5>
-//               <p className="card-text">
-//                 Experience the joy of play to relax and rejuvenate! Dive into
-//                 our games for a refreshing escape.
-//               </p>
-//               <Link to="/games" target="_blank">
-//                 <Button>Play Now</Button>
-//               </Link>
-//             </div>
-//           </div>
-
-//           <div className="card" style={{ width: "18rem" }}>
-//             <img src={img3} className="card-img-top" alt="..." />
-//             <div className="card-body">
-//               <h5 className="card-title">Chat Bot</h5>
-//               <p className="card-text">
-//                 Engage in a lively chat with our friendly bot for a fun and
-//                 helpful interaction!
-//               </p>
-//               <Link to="https://emowellbeing.streamlit.app/" target="_blank">
-//                 <Button>Chat Now</Button>
-//               </Link>
-//               {/* <a href="https://emowellbeing.streamlit.app/" className="btn btn-primary">Chat Now</a> */}
-//             </div>
-//           </div>
-//           <div className="card">
-//             <img src={img3} className="card-img-top" alt="..." />
-//             <div className="card-body">
-//               <h5 className="card-title">Community Forum</h5>
-//               <p className="card-text">
-//                 Engage in a lively chat with our friendly bot for a fun and
-//                 helpful interaction!
-//               </p>
-//               {isAuthenticated ? (
-//                 <Link to="./forum" target="_blank">
-//                   <Button>Learn More</Button>
-//                 </Link>
-//               ) : (
-//                 <Button onClick={() => loginWithRedirect()}>Learn More</Button>
-//               )}
-//             </div>
-//           </div>
-//         </div>
-//       </Services>
-//     </>
-//   );
-// };
-
-// export default Features;
-
 import { useAuth0 } from "@auth0/auth0-react";
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
 
-// Styled components for section, h2, p, img, and anchor elements
 const Section = styled.section`
   padding: 4rem 0 2rem;
   display: flex;
@@ -223,9 +33,9 @@ const Container = styled.div`
 
 const DecorationContainer = styled.div`
 display: flex;
-flex-wrap: wrap; /* Allow cards to wrap to the next row */
-gap: 2rem; /* Add gap between cards */
-justify-content: center; /* Center the cards horizontally */
+flex-wrap: wrap;
+gap: 2rem;
+justify-content: center;
 `;
 
 const DecorationData = styled.div`
@@ -234,10 +44,10 @@ padding: 1rem 1rem 2rem;
 background-color: var(--container-color);
 box-shadow: 0 2px 6px rgba(65, 11, 16, 0.15);
 border-radius: 1rem;
-width: calc(25% - 1rem); /* Adjust width to occupy 25% of the container width */
-max-width: 220px; /* Limit maximum width */
-flex: 1 0 auto; /* Allow flex-grow and prevent shrinking */
-box-sizing: border-box; /* Include padding and border in the width */
+width: calc(25% - 1rem);
+max-width: 220px;
+flex: 1 0 auto;
+box-sizing: border-box;
 `;
 
 const DecorationImg = styled.img`
@@ -268,25 +78,12 @@ const Button = styled.div`
   }
 `;
 
-const ButtonLink = styled.a`
-  background: none;
-  padding: 0;
-  color: var(--first-color);
-
-  &:hover {
-    background-color: transparent;
-    color: var(--first-color-alt);
-  }
-`;
-
-// Replace the original JSX with styled components
 const FeaturesSection = () => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
   return (
     <Section>
       <Title> Our Features</Title>
       <Container>
-        {/* Place your cards on the right side here */}
         <DecorationContainer>
           <DecorationData>
             <DecorationImg
@@ -296,11 +93,11 @@ const FeaturesSection = () => {
             <DecorationTitle>Reading Area</DecorationTitle>
             <p>Ready to unwind and let loose? Discover actionable mental health
                  and wellbeing advice.</p>
-            <Button>
-              <ButtonLink href="./readingArea" target="_blank">Read Now</ButtonLink>
-            </Button>
+
+              <Link to="/readingarea" target="_blank">
+                  <Button>Read Now</Button>
+                </Link>
           </DecorationData>
-          {/* Repeat the same pattern for other elements */}
           <DecorationData>
             <DecorationImg
               src={img2} 
@@ -309,9 +106,9 @@ const FeaturesSection = () => {
             <DecorationTitle>Explore Games</DecorationTitle>
             <p>Experience the joy of play to relax and rejuvenate! Dive into
                  our games for a refreshing escape.</p>
-            <Button>
-              <ButtonLink href="/games" target="_blank">Play Now</ButtonLink>
-            </Button>
+              <Link to="/games" target="_blank">
+                  <Button>Play Now</Button>
+                </Link>
           </DecorationData>
 
           <DecorationData>
@@ -322,9 +119,9 @@ const FeaturesSection = () => {
             <DecorationTitle>Chat Bot</DecorationTitle>
             <p>Engage in a lively chat with our friendly bot for a fun and
                  helpful interaction!</p>
-            <Button>
-              <ButtonLink href="https://emowellbeing.streamlit.app/" target="_blank">Chat Now</ButtonLink>
-            </Button>
+              <Link to="https://emowellbeing.streamlit.app/" target="_blank">
+                  <Button>Chat Now</Button>
+                </Link>
           </DecorationData>
 
           <DecorationData>
--- src/config/data.js
+++ src/config/data.js
@@ -2,7 +2,7 @@ export const blogList = [
     {
       id: 1,
       title: 'UEmbracing Balance: Tips for Mental Well-Being',
-      category: 'Cervical Health Awareness',
+      category: 'Mental Health Awareness',
       description:
         "Dive deep into the complexities of the human mind as we uncover the latest research, trends, and developments in mental health. From neuroscience to psychology, this blog explores the multifaceted nature of mental health, shedding light on topics that impact us all.",
       cover: 'https://media.istockphoto.com/id/1294477039/vector/metaphor-bipolar-disorder-mind-mental-double-face-split-personality-concept-mood-disorder-2.jpg?s=612x612&w=0&k=20&c=JtBxyFapXIA63hzZk_F5WNDF92J8fD2gIFNX3Ta4U3A=',
@@ -11,7 +11,7 @@ export const blogList = [
     {
       id: 2,
       title: 'Mind Matters: Exploring the Depths of Mental Health',
-      category: 'Women’s Health',
+      category: 'Mental Health',
       description:
         "Hear the voices of courage and resilience as individuals share their journeys toward mental wellness. These raw and honest narratives offer hope, inspiration, and solidarity, encouraging readers to break free from stigma and embrace their own paths to healing.",
       cover: 'https://www.shutterstock.com/image-photo/mental-health-bulb-word-cloud-260nw-282943754.jpg',
@@ -20,7 +20,7 @@ export const blogList = [
     {
       id: 3,
       title: 'Breaking the Silence: Stories of Mental Wellness',
-      category: 'HPV Awareness',
+      category: 'Combating Mental Health Issues',
       description:
         " Delve into the intricate landscape of mental health with expert insights, personal anecdotes, and evidence-based strategies. Explore topics ranging from anxiety and depression to resilience and self-care, gaining valuable perspectives and practical advice along the way.",
       cover: 'https://media.istockphoto.com/id/1513072392/photo/hands-holding-paper-head-human-brain-with-flowers-self-care-and-mental-health-concept.webp?b=1&s=170667a&w=0&k=20&c=AWKtEsKj6IVrr0zALEGZ933cGXNeDf8ExWwtmon-xfo=',
@@ -29,7 +29,7 @@ export const blogList = [
     {
       id: 4,
       title: 'Navigating the Mind: Insights on Mental Health',
-      category: 'Cervical Health Support',
+      category: 'Mental Health Support',
       description:
         "Dive deep into the complexities of the human mind as we uncover the latest research, trends, and developments in mental health. From neuroscience to psychology, this blog explores the multifaceted nature of mental health, shedding light on topics that impact us all.",
       cover: 'https://wallpapers.com/images/hd/mental-health-pictures-i8ov8ws3hfw2v4ix.jpg',
@@ -38,7 +38,7 @@ export const blogList = [
     {
       id: 5,
       title: 'Mind Matters: Exploring the Depths of Mental Health',
-      category: 'Cancer Awareness',
+      category: 'Mental Health Awareness',
       description:
         " Delve into the intricate landscape of mental health with expert insights, personal anecdotes, and evidence-based strategies. Explore topics ranging from anxiety and depression to resilience and self-care, gaining valuable perspectives and practical advice along the way.",
       cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY0zG-A8dNiN7pB0ucRaTKBlHJQ1CTwhsJdtAFGvcaZg&s',
@@ -46,19 +46,19 @@ export const blogList = [
     },
     {
       id: 6,
-      title: 'Advances in Cervical Cancer Treatment: Innovations and Research Breakthroughs',
-      category: 'Cervical Cancer Awareness',
+      title: 'Mindful Moments: Cultivating Peace in a Busy World',
+      category: 'Mental Health Awareness',
       description:
-        "Hear the voices of courage and resilience as individuals share their journeys toward mental wellness. These raw and honest narratives offer hope, inspiration, and solidarity, encouraging readers to break free from stigma and embrace their own paths to healing.",
-  cover: 'https://res.cloudinary.com/dx0dgujbj/image/upload/v1707697211/CerviCare/Screenshot_2024-02-12_054937_lx3yuu.png',
+        "Take a moment to pause and reflect with our blog, where we explore the power of mindfulness in promoting mental clarity and inner peace. Discover practices to help you stay grounded and present in the midst of life's challenges.",
+  cover: 'https://st4.depositphotos.com/1229718/31571/i/450/depositphotos_315716008-stock-photo-mental-health-support.jpg',
     backgroundColor: "#FF6663",
   },
   {
     id: 7,
-      title: 'Living with Cervical Cancer: Coping Strategies and Support Resources',
-        category: 'Cervical Cancer Awareness',
+      title: 'How To Change Your Behaviors & Thoughts',
+        category: 'Mental Health Awareness',
           description:
-    " Delve into the intricate landscape of mental health with expert insights, personal anecdotes, and evidence-based strategies. Explore topics ranging from anxiety and depression to resilience and self-care, gaining valuable perspectives and practical advice along the way.",
+    " Changing your behavior requires an understanding of what is triggering you, causing your beliefs and behaviors, and leading to negative or positive consequences. Without this understanding it will be hard to change what is causing the problem.",
       cover: 'https://png.pngtree.com/png-clipart/20211014/ourmid/pngtree-mental-health-problems-flat-illustration-anxiety-disorder-png-image_3977575.png',
         backgroundColor: "#CC99C9",
   }
