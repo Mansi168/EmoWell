2c026958ce8b8cd3e2a16a8471ceb5c9ebb68ed5
Monalika Patnaik <monalikapatnaik114@gmail.com>
2024-04-02 19:04:42 +0530

    minor-changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  // optimizeDeps: {
+  //   exclude: ['nextjs-websocket']
+  // }
 
 })
