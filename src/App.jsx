b6df3723c2e9a8644b368d1940aab6ecce589fe1
Manav Malhotra <manavmalhotra173@gmail.com>
2024-05-19 16:15:57 +0530

    Added a Review Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Pages/Home";
 import ContactPage from './Pages/Contact'
+import ReviewPage from './Pages/Review'
 import ChatComponent from './components/Forum';
 import ReadingArea from './Pages/ReadingArea';
 import Games from './Pages/Games';
@@ -18,6 +19,7 @@ function App() {
     <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/contact" element={<ContactPage />} />
+    <Route path="/review" element={<ReviewPage />} />
     <Route path="/forum" element={<ChatComponent />} />
     <Route path='/readingarea' element={<ReadingArea/>} />
     <Route path='/games' element={<Games/>} />
