d17cd0eb67a3c99b0451afbd147d620913795ac0
Nikhilesh <nikhileshg02@gmail.com>
2024-05-21 13:17:44 +0530

    removed bootstrap cdn and added via npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
