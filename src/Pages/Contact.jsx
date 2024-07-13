60285cc78e2fc62316616e44d585620401321a80
Dhairya Gothi <dhairyag31@gmail.com>
2024-07-13 18:54:54 +0530

    added footer

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,13 +1,14 @@
 
 import Navbar from "../components/Navbar/Navbar";
 import Contact from "../components/Contact/contact";
+import Footer from "../components/Footer/Footer";
 
 const ContactPage = () => {
   return (
     <>
       <Navbar />
       <Contact />
-     
+     <Footer/>
     </>
   );
 };
