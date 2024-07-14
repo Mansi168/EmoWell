2aefb95914c27407cc4221768573ff8375be9d69
pani2004 <debashispani61@gmail.com>
2024-07-14 15:33:25 +0530

    Imported the footer and the navbar

--- src/components/community/CommunityPage.jsx
+++ src/components/community/CommunityPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { css } from '@emotion/react';
 import { ClipLoader } from 'react-spinners';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 import './CommunityPage.css';
 
 const override = css`
@@ -20,8 +22,6 @@ const CommunityPage = () => {
   const token = import.meta.env.token; 
 
   const headers = token ? { Authorization: `token ${token}` } : {};
-
-  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -52,6 +52,7 @@ const CommunityPage = () => {
     };
     fetchData();
   }, []);
+
   useEffect(() => {
     window.scrollTo(0, 0); 
   }, []);
@@ -60,86 +61,100 @@ const CommunityPage = () => {
     window.location.href = `https://github.com/${owner}/${repo}/contributors`;
   };
 
+  const contributeOnGitHub = () => {
+    window.location.href = `https://github.com/${owner}/${repo}`;
+  };
+
   return (
-    <div className="community-page">
-      {loading ? (
-        <div className="loading-spinner">
-          <ClipLoader color="#318f5f" loading={loading} css={override} size={150} />
-        </div>
-      ) : (
-        <div className="community-container">
-          <h1 className="title">EmoWell's Community</h1>
-          <div className="grid">
-            <div className="card">
-              <h2>Stars</h2>
-              <p>{repoData.stars}</p>
-            </div>
-            <div className="card">
-              <h2>Forks</h2>
-              <p>{repoData.forks}</p>
-            </div>
-            <div className="card">
-              <h2>Commits</h2>
-              <p>{repoData.commits}</p>
-            </div>
-            <div className="card">
-              <h2>Open Issues</h2>
-              <p>{repoData.openIssues}</p>
-            </div>
-            <div className="card">
-              <h2>Closed Issues</h2>
-              <p>{repoData.closedIssues}</p>
-            </div>
-            <div className="card">
-              <h2>Open PRs</h2>
-              <p>{repoData.openPRs}</p>
-            </div>
-            <div className="card">
-              <h2>Closed PRs</h2>
-              <p>{repoData.closedPRs}</p>
-            </div>
-            <div className="card">
-              <h2>Contributors</h2>
-              <p>{repoData.contributors}</p>
-            </div>
+    <div>
+      <Navbar />
+      <div className="community-page">
+        {loading ? (
+          <div className="loading-spinner">
+            <ClipLoader color="#318f5f" loading={loading} css={override} size={150} />
           </div>
-
-          <div className="contributors-container">
-            <h1 className="contributors-title">Top Contributors</h1>
-            <div className="contributors-grid">
-              {contributors.map((contributor) => (
-                <div key={contributor.id} className="contributor-card">
-                  <a
-                    href={contributor.html_url}
-                    className="contributor-link"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      src={contributor.avatar_url}
-                      alt={contributor.login}
-                      className="contributor-avatar"
-                    />
-                  </a>
-                  <h2 className="contributor-name">{contributor.login}</h2>
-                  <p className="contributor-contributions">
-                    Contributions: {contributor.contributions}
-                  </p>
-                </div>
-              ))}
+        ) : (
+          <div className="community-container">
+            <h1 className="title">EmoWell's Community</h1>
+            <div className="grid">
+              <div className="card">
+                <h2>Stars</h2>
+                <p>{repoData.stars}</p>
+              </div>
+              <div className="card">
+                <h2>Forks</h2>
+                <p>{repoData.forks}</p>
+              </div>
+              <div className="card">
+                <h2>Commits</h2>
+                <p>{repoData.commits}</p>
+              </div>
+              <div className="card">
+                <h2>Open Issues</h2>
+                <p>{repoData.openIssues}</p>
+              </div>
+              <div className="card">
+                <h2>Closed Issues</h2>
+                <p>{repoData.closedIssues}</p>
+              </div>
+              <div className="card">
+                <h2>Open PRs</h2>
+                <p>{repoData.openPRs}</p>
+              </div>
+              <div className="card">
+                <h2>Closed PRs</h2>
+                <p>{repoData.closedPRs}</p>
+              </div>
+              <div className="card">
+                <h2>Contributors</h2>
+                <p>{repoData.contributors}</p>
+              </div>
             </div>
-          </div>
+            <div className="button-container">
+               <button className="contribute-btn" onClick={contributeOnGitHub}>
+                Contribute on GitHub
+              </button>
+            </div>
+            {/* Contributor's Section*/ }
+            <div className="contributors-container">
+              <h1 className="contributors-title">Top Contributors</h1>
+              <div className="contributors-grid">
+                {contributors.map((contributor) => (
+                  <div key={contributor.id} className="contributor-card">
+                    <a
+                      href={contributor.html_url}
+                      className="contributor-link"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        src={contributor.avatar_url}
+                        alt={contributor.login}
+                        className="contributor-avatar"
+                      />
+                    </a>
+                    <h2 className="contributor-name">{contributor.login}</h2>
+                    <p className="contributor-contributions">
+                      Contributions: {contributor.contributions}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
 
-          <button className="view-contributors-btn" onClick={viewAllContributors}>
-            View All Contributors
-          </button>
-        </div>
-      )}
+            <button className="view-contributors-btn" onClick={viewAllContributors}>
+              View All Contributors
+            </button>
+          </div>
+        )}
+      </div>
+      <Footer/>
     </div>
   );
 };
 
 export default CommunityPage;
+
 
 
 
