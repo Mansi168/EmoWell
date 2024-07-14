16e4181c666a307a65529d2b07ea6ce913a597e3
Priyamvada Shah <priyamvadashahlko@gmail.com>
2024-07-14 18:24:50 +0530

    final

--- src/config/data.js
+++ src/config/data.js
@@ -1,66 +1,68 @@
 export const blogList = [
-    {
-      id: 1,
-      title: 'UEmbracing Balance: Tips for Mental Well-Being',
-      category: 'Mental Health Awareness',
-      description:
-        "Dive deep into the complexities of the human mind as we uncover the latest research, trends, and developments in mental health. From neuroscience to psychology, this blog explores the multifaceted nature of mental health, shedding light on topics that impact us all.",
-      cover: 'https://media.istockphoto.com/id/1294477039/vector/metaphor-bipolar-disorder-mind-mental-double-face-split-personality-concept-mood-disorder-2.jpg?s=612x612&w=0&k=20&c=JtBxyFapXIA63hzZk_F5WNDF92J8fD2gIFNX3Ta4U3A=',
-      backgroundColor: "#FF6663",
-    },
-    {
-      id: 2,
-      title: 'Mind Matters: Exploring the Depths of Mental Health',
-      category: 'Mental Health',
-      description:
-        "Hear the voices of courage and resilience as individuals share their journeys toward mental wellness. These raw and honest narratives offer hope, inspiration, and solidarity, encouraging readers to break free from stigma and embrace their own paths to healing.",
-      cover: 'https://www.shutterstock.com/image-photo/mental-health-bulb-word-cloud-260nw-282943754.jpg',
-      backgroundColor: "#FEB144",
-    },
-    {
-      id: 3,
-      title: 'Breaking the Silence: Stories of Mental Wellness',
-      category: 'Combating Mental Health Issues',
-      description:
-        " Delve into the intricate landscape of mental health with expert insights, personal anecdotes, and evidence-based strategies. Explore topics ranging from anxiety and depression to resilience and self-care, gaining valuable perspectives and practical advice along the way.",
-      cover: 'https://media.istockphoto.com/id/1513072392/photo/hands-holding-paper-head-human-brain-with-flowers-self-care-and-mental-health-concept.webp?b=1&s=170667a&w=0&k=20&c=AWKtEsKj6IVrr0zALEGZ933cGXNeDf8ExWwtmon-xfo=',
-      backgroundColor: "#f8d568",
-    },
-    {
-      id: 4,
-      title: 'Navigating the Mind: Insights on Mental Health',
-      category: 'Mental Health Support',
-      description:
-        "Dive deep into the complexities of the human mind as we uncover the latest research, trends, and developments in mental health. From neuroscience to psychology, this blog explores the multifaceted nature of mental health, shedding light on topics that impact us all.",
-      cover: 'https://wallpapers.com/images/hd/mental-health-pictures-i8ov8ws3hfw2v4ix.jpg',
-      backgroundColor: '#FF6663',
-    },
-    {
-      id: 5,
-      title: 'Mind Matters: Exploring the Depths of Mental Health',
-      category: 'Mental Health Awareness',
-      description:
-        " Delve into the intricate landscape of mental health with expert insights, personal anecdotes, and evidence-based strategies. Explore topics ranging from anxiety and depression to resilience and self-care, gaining valuable perspectives and practical advice along the way.",
-      cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY0zG-A8dNiN7pB0ucRaTKBlHJQ1CTwhsJdtAFGvcaZg&s',
-      backgroundColor: "#f8d568",
-    },
-    {
-      id: 6,
-      title: 'Mindful Moments: Cultivating Peace in a Busy World',
-      category: 'Mental Health Awareness',
-      description:
-        "Take a moment to pause and reflect with our blog, where we explore the power of mindfulness in promoting mental clarity and inner peace. Discover practices to help you stay grounded and present in the midst of life's challenges.",
-  cover: 'https://st4.depositphotos.com/1229718/31571/i/450/depositphotos_315716008-stock-photo-mental-health-support.jpg',
+  {
+    id: 1,
+    title: "Embracing Balance: Tips for Mental Well-Being",
+    category: "Mental Health Awareness",
+    description:
+      "Dive deep into the complexities of the human mind as we uncover the latest research, trends, and developments in mental health. From neuroscience to psychology, this blog explores the multifaceted nature of mental health, shedding light on topics that impact us all.",
+    cover:
+      "https://media.istockphoto.com/id/1294477039/vector/metaphor-bipolar-disorder-mind-mental-double-face-split-personality-concept-mood-disorder-2.jpg?s=612x612&w=0&k=20&c=JtBxyFapXIA63hzZk_F5WNDF92J8fD2gIFNX3Ta4U3A=",
     backgroundColor: "#FF6663",
+    link: "https://mhanational.org/31-tips-boost-your-mental-health",
   },
   {
-    id: 7,
-      title: 'How To Change Your Behaviors & Thoughts',
-        category: 'Mental Health Awareness',
-          description:
-    " Changing your behavior requires an understanding of what is triggering you, causing your beliefs and behaviors, and leading to negative or positive consequences. Without this understanding it will be hard to change what is causing the problem.",
-      cover: 'https://png.pngtree.com/png-clipart/20211014/ourmid/pngtree-mental-health-problems-flat-illustration-anxiety-disorder-png-image_3977575.png',
-        backgroundColor: "#CC99C9",
-  }
-  
-  ]
+    id: 2,
+    title: "Mind Matters: Exploring the Depths of Mental Health",
+    category: "Mental Health",
+    description:
+      "Hear the voices of courage and resilience as individuals share their journeys toward mental wellness. These raw and honest narratives offer hope, inspiration, and solidarity, encouraging readers to break free from stigma and embrace their own paths to healing.",
+    cover:
+      "https://www.shutterstock.com/image-photo/mental-health-bulb-word-cloud-260nw-282943754.jpg",
+    backgroundColor: "#FEB144",
+    link: "https://www.helpguide.org/articles/mental-health/building-better-mental-health.htm",
+  },
+  {
+    id: 3,
+    title: "Breaking the Silence: Stories of Mental Wellness",
+    category: "Combating Mental Health Issues",
+    description:
+      " Delve into the intricate landscape of mental health with expert insights, personal anecdotes, and evidence-based strategies. Explore topics ranging from anxiety and depression to resilience and self-care, gaining valuable perspectives and practical advice along the way.",
+    cover:
+      "https://media.istockphoto.com/id/1513072392/photo/hands-holding-paper-head-human-brain-with-flowers-self-care-and-mental-health-concept.webp?b=1&s=170667a&w=0&k=20&c=AWKtEsKj6IVrr0zALEGZ933cGXNeDf8ExWwtmon-xfo=",
+    backgroundColor: "#f8d568",
+    link: "https://www.indiatvnews.com/health/7-ways-to-break-the-silence-surrounding-mental-health-issues-2023-10-02-895810#:~:text=By%20educating%20ourselves%20and%20others,the%20silence%20surrounding%20mental%20health.",
+  },
+  {
+    id: 4,
+    title: "Navigating the Mind: Insights on Mental Health",
+    category: "Mental Health Support",
+    description:
+      "Dive deep into the complexities of the human mind as we uncover the latest research, trends, and developments in mental health. From neuroscience to psychology, this blog explores the multifaceted nature of mental health, shedding light on topics that impact us all.",
+    cover:
+      "https://wallpapers.com/images/hd/mental-health-pictures-i8ov8ws3hfw2v4ix.jpg",
+    backgroundColor: "#FF6663",
+    link: "https://www.mayoclinic.org/diseases-conditions/mental-illness/symptoms-causes/syc-20374968",
+  },
+  {
+    id: 5,
+    title: "Mind Matters: Exploring the Depths of Mental Health",
+    category: "Mental Health Awareness",
+    description:
+      " Delve into the intricate landscape of mental health with expert insights, personal anecdotes, and evidence-based strategies.",
+    cover:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY0zG-A8dNiN7pB0ucRaTKBlHJQ1CTwhsJdtAFGvcaZg&s",
+    backgroundColor: "#f8d568",
+    link: "https://www.mentalhealth.org.uk/explore-mental-health/publications/our-best-mental-health-tips",
+  },
+  {
+    id: 6,
+    title: "Mindful Moments: Cultivating Peace in a Busy World",
+    category: "Mental Health Awareness",
+    description:
+      "Take a moment to pause and reflect with our blog, where we explore the power of mindfulness in promoting mental clarity and inner peace. Discover practices to help you stay grounded and present in the midst of life's challenges.",
+    cover:
+      "https://st4.depositphotos.com/1229718/31571/i/450/depositphotos_315716008-stock-photo-mental-health-support.jpg",
+    backgroundColor: "#FF6663",
+    link: "https://the-qi.com/blogs/journal/finding-peace-in-a-busy-world",
+  },
+];
---
+++ src/config/depressionData.js
@@ -0,0 +1,35 @@
+export const depressionBlogList = [
+    {
+      id: 7,
+      title: "A guide: Cope with depression",
+      category: "Depression Awareness",
+      description:
+        " Depression is a mental health condition that causes a chronic feeling of emptiness, sadness, or inability to feel pleasure that may appear to happen for no clear reason.",
+      cover:
+        "https://png.pngtree.com/png-clipart/20211014/ourmid/pngtree-mental-health-problems-flat-illustration-anxiety-disorder-png-image_3977575.png",
+      backgroundColor: "#CC99C9",
+      link: "https://www.who.int/news-room/fact-sheets/detail/depression#:~:text=Depressive%20disorder%20(also%20known%20as,and%20feelings%20about%20everyday%20life.",
+    },
+    {
+      id: 8,
+      title: "Try these strategies if you are feeling depressed.",
+      category: "Depression Treatment",
+      description:
+        " Changing your behavior requires an understanding of what is triggering you, causing your beliefs and behaviors, and leading to negative or positive consequences. Without this understanding it will be hard to change what is causing the problem.",
+      cover:"https://t3.ftcdn.net/jpg/04/00/38/86/360_F_400388679_dmUTdbK8sn5SSuP34VUrnaZAIZWysGKL.jpg",
+      backgroundColor: "#CC99C9",
+      link: "https://www.nhs.uk/mental-health/self-help/tips-and-support/cope-with-depression/",
+    },
+    {
+      id: 9,
+      title: "A serotonin theory of depression",
+      category: "Depression Theory",
+      description:
+        "The idea that depression is the result of abnormalities in brain chemicals, particularly serotonin (5-hydroxytryptamine or 5-HT), has been influential for decades, and provides an important justification for the use of antidepressants.",
+      cover:
+        "https://t3.ftcdn.net/jpg/03/23/76/22/360_F_323762246_EXXTDchc6R77qkOljy1J6tm1FuzA4Td9.jpg",
+      backgroundColor: "#CC99C9",
+      link: "https://www.nature.com/articles/s41380-022-01661-0",
+    },
+  ];
+  
