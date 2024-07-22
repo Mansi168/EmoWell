46f0558cda9b0881975b43221ea0d4b2039a2342
Radhika Malpani <radhikavishnudas2002@gmail.com>
2024-07-22 19:56:06 +0530

    Added scroll top button main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react';
+import FloatBtn from './components/FloatBtn/FloatBtn.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Auth0Provider
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   }}
 >
   <App />
+  <FloatBtn />
 </Auth0Provider> 
 )
