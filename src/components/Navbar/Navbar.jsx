081fa257e1af861e807cb95ffc1aabc431a8ceb7
Sayyad Aslam <sayyadaslam2020@gmail.com>
2024-07-31 22:40:39 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,10 +8,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 import logo from "../../assets/logo.png";
 import styled from "styled-components";
 
-import ProgressBar from "./Progressbar";
-
-
-
 const Button = styled.button`
   padding: 5px 24px;
   font-style: normal;
@@ -42,38 +38,26 @@ const Navbar = () => {
   };
 
   const handleLogoClick = () => {
-    setIsOpen(!isOpen);
     window.scrollTo(0, 0);
   };
 
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
-      const yOffset = -120; // Adjust this value as needed
-      const yPosition = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
-      window.scrollTo({ top: yPosition, behavior: 'smooth' });
+      section.scrollIntoView({ behavior: "smooth" });
     }
   };
 
   const handleNavClick = (path, sectionId) => {
     navigate(path);
-    setIsOpen(false);
     setTimeout(() => {
       scrollToSection(sectionId);
     }, 100);
   };
 
   return (
-      <>
-      <Nav
-        style={{
-          background: "#2B394A",
-        }}
-      >
-        
-
+    <>
       <Nav>
-
         <NavbarContainer>
           <NavImg>
             <Link to="/" onClick={handleLogoClick}>
@@ -90,20 +74,50 @@ const Navbar = () => {
           </MobileIcon>
 
           <NavMenu isOpen={isOpen}>
-          
-            
+            <NavItem>
+              <NavLinks to="/" onClick={handleLogoClick}>Home</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/#about" onClick={() => handleNavClick('/', 'about')}>About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/#Features" onClick={() => handleNavClick('/', 'featureSection')}>Features</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/contact" onClick={handleLogoClick}>Contact</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="/review" onClick={handleLogoClick}>Review</NavLinks>
+            </NavItem>
+            {isAuthenticated ? (
+              <li>
+                <Dropdown>
+                  <FontAwesomeIcon icon={faUser} style={{ color: '#fff' }} />
+                  <ButtonDropdown onClick={toggleDropdown}>Account
+                    <FontAwesomeIcon icon={faCaretDown} style={{ color: '#fff' }} />
+                  </ButtonDropdown>
+                  {isOpen && (
+                    <DropdownContent>
+                      <a href="/profile">User Profile</a>
+                      <button onClick={() => logout({ returnTo: window.location.origin })} style={{ fontSize: '16px', fontWeight: '600' }}>
+                        Log Out
+                      </button>
+                    </DropdownContent>
+                  )}
+                </Dropdown>
+              </li>
+            ) : (
+              <li>
+                <Button onClick={() => loginWithRedirect()} style={{ border: '1px solid rgb(48,175,91)' }}>
+                  Register
+                </Button>
+              </li>
+            )}
           </NavMenu>
-          
         </NavbarContainer>
-        
       </Nav>
-      <ProgressBar/>
-    
-    </Nav>
-      <ProgressBar/>
     </>
   );
 };
-
 
 export default Navbar;
